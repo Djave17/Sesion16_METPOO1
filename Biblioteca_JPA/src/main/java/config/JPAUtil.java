1aad075281795312c0e50594e7c527e1857a9530
Djave17 <dsancheza@uamv.edu.ni>
2025-10-16 23:41:44 -0600

    Listo

--- Biblioteca_JPA/src/main/java/config/JPAUtil.java
+++ Biblioteca_JPA/src/main/java/config/JPAUtil.java
@@ -5,7 +5,7 @@ import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
 public class JPAUtil {
-    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("bibliotecaDB");
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("bibliotecaJPA");
 
     public static EntityManager getEntityManager() {
         return emf.createEntityManager();
